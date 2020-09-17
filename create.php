d16f34aa0436d47c4a9902aba020af0742317796
Rumon <crash.msr@gmail.com>
2020-09-17 20:46:55 +0600

    fixed: removed 1 extra placeholder

--- create.php
+++ create.php
@@ -2,7 +2,7 @@
 
 $token = bin2hex(random_bytes(5));
 $pdo = require './_pdo.php';
-$stmt = $pdo->prepare('INSERT INTO `Users` (`FirstName`, `LastName`, `Email`, `Password`, `Token`) VALUES (?, ?, ?, ?, ?, ?)');
+$stmt = $pdo->prepare('INSERT INTO `Users` (`FirstName`, `LastName`, `Email`, `Password`, `Token`) VALUES (?, ?, ?, ?, ?)');
 $stmt->execute([$_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['password1'], $token]);
 // send mail
 header('Location: signin.php');
